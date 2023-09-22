5e7258ead17566b6c856d1ffff4728f2bcf8d9a5
Igor Belyaletdinov <igor.belyaletdinov@studio-moderna.com>
2023-09-22 13:26:20 +0200

    Added missing files

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,19 @@
+import { LoadStrategy } from '@mikro-orm/core';
+import { defineConfig } from '@mikro-orm/mysql';
+import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
+import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
+import { Migrator } from '@mikro-orm/migrations';
+import { EntityGenerator } from '@mikro-orm/entity-generator';
+import { SeedManager } from '@mikro-orm/seeder';
+
+export default defineConfig({
+  entities: ['dist/**/*.entity.js'],
+  entitiesTs: ['src/**/*.entity.ts'],
+  debug: true,
+  loadStrategy: LoadStrategy.JOINED,
+  highlighter: new SqlHighlighter(),
+  metadataProvider: TsMorphMetadataProvider,
+  // @ts-expect-error nestjs adapter option
+  registerRequestContext: false,
+  extensions: [Migrator, EntityGenerator, SeedManager],
+});
