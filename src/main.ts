82e491e3fd9b5db954322dc0d3ace2c0633bab04
Igor Belyaletdinov <igor.belyaletdinov@studio-moderna.com>
2023-10-02 10:50:43 +0200

    Option to run without SSL.

--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,59 @@
 import {NestApplication, NestFactory} from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-
 import {ExpressAdapter} from "@nestjs/platform-express";
 import {Server} from "https";
 import * as fs from "fs";
 import express, {Express} from 'express';
 import spdy from 'spdy';
 import {ServerOptions} from "spdy";
+import {USE_SSL} from "./config";
 
 async function bootstrap() {
-  // const appOptions = { cors: true };
-  // const app = await NestFactory.create(AppModule, appOptions);
-  // app.setGlobalPrefix('api');
-  //
-  // const options = new DocumentBuilder()
-  //   .setTitle('SIA Data Wallet API')
-  //   .setDescription('SIA Data Wallet API description')
-  //   .setVersion('1.0')
-  //   .addBearerAuth()
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, options);
-  // SwaggerModule.setup('/docs', app, document);
-  //
-  // await app.listen(3000);
-
-  // Alternative to spdy server: https://docs.nestjs.com/faq/multiple-servers#multiple-simultaneous-servers
-  const expressApp: Express = express();
-
-  // https://github.com/spdy-http2/node-spdy
-  const spdyOpts: ServerOptions = {
-    key: fs.readFileSync('./test.key'),
-    cert: fs.readFileSync('./test.crt'),
-  };
-
-  const server: Server = spdy.createServer(spdyOpts, expressApp);
-
-  const appOptions = { cors: true };
-
-  const app: NestApplication = await NestFactory.create(
-      AppModule,
-      new ExpressAdapter(expressApp),
-      appOptions
-  );
-  app.setGlobalPrefix('api');
-
   const options = new DocumentBuilder()
-    .setTitle('SIA Data Wallet API')
-    .setDescription('SIA Data Wallet API description')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('/docs', app, document);
+      .setTitle('SIA Data Wallet API')
+      .setDescription('SIA Data Wallet API description')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
 
-  await app.init();
-  await server.listen(443);
+  if (USE_SSL === 'false') {
+    const appOptions = { cors: true };
+    const app = await NestFactory.create(AppModule, appOptions);
+    app.setGlobalPrefix('api');
+
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('/docs', app, document);
+
+    await app.listen(3003);
+  } else {
+    // Alternative to spdy server: https://docs.nestjs.com/faq/multiple-servers#multiple-simultaneous-servers
+    const expressApp: Express = express();
+
+    // https://github.com/spdy-http2/node-spdy
+    const spdyOpts: ServerOptions = {
+      key: fs.readFileSync('./test.key'),
+      cert: fs.readFileSync('./test.crt'),
+    };
+
+    const server: Server = spdy.createServer(spdyOpts, expressApp);
+
+    const appOptions = {cors: true};
+
+    const app: NestApplication = await NestFactory.create(
+        AppModule,
+        new ExpressAdapter(expressApp),
+        appOptions
+    );
+    app.setGlobalPrefix('api');
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('/docs', app, document);
+
+    await app.init();
+    await server.listen(443);
+  }
 }
 bootstrap()
   .catch((err) => {
