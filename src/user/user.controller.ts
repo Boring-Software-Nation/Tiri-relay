bff346b0bd77fb54f2ce8b21d6c734f1d633e998
Igor Belyaletdinov <igor.belyaletdinov@studio-moderna.com>
2023-10-27 12:31:58 +0200

    Renew same subscriptions. Upgrade/downgrade.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -60,8 +60,8 @@ export class UserController {
   @UsePipes(new ValidationPipe())
   @Post('users/subscribe')
   async subscribe(@Body('subscription') subscribeUserDto: SubscribeUserDto): Promise<any> {
-    let totalToPay : number = parseFloat(subscribeUserDto.subscriptionPrice) ;
-    let subscriptionDate: string = '';
+    let totalToPay : number = 0, alreadyPaid : number = 0
+    let subscriptionDate: string = '', subscriptionStartedDate: Date;
     try {
       if (subscribeUserDto.subscriptionAddress !== SUBSCRIPTION_PAY_ADDRESS ||
           (subscribeUserDto.subscriptionCode === 'SMALL' && subscribeUserDto.subscriptionPrice != SMALL_PLAN_PRICE) ||
@@ -71,14 +71,35 @@ export class UserController {
         return {status: 400, statusText: 'Bad Request', data: {error: ['Invalid subscription data']}}
       }
 
+      if (subscribeUserDto.subscriptionCode === 'SMALL') {
+        totalToPay = parseFloat(SMALL_PLAN_PRICE)
+      } else if (subscribeUserDto.subscriptionCode === 'MEDIUM') {
+        totalToPay = parseFloat(MEDIUM_PLAN_PRICE)
+      } else if (subscribeUserDto.subscriptionCode === 'LARGE') {
+        totalToPay = parseFloat(LARGE_PLAN_PRICE)
+      }
+
+      console.log('totalToPay full', totalToPay)
+
       const subscriptionsData = await apiLago.subscriptions.findAllSubscriptions({
             external_customer_id: subscribeUserDto.wallet,
           }
       )
 
-      let code = subscribeUserDto.subscriptionCode
+      let code = subscribeUserDto.subscriptionCode;
+      let wasPreviousSubscription = false;
       if (subscriptionsData.data.subscriptions.length > 0) {
         const currentSubscription = subscriptionsData.data.subscriptions[0];
+        wasPreviousSubscription = true;
+
+        if (currentSubscription.plan_code === 'SMALL') {
+          alreadyPaid = parseFloat(SMALL_PLAN_PRICE)
+        } else if (currentSubscription.plan_code === 'MEDIUM') {
+          alreadyPaid = parseFloat(MEDIUM_PLAN_PRICE)
+        } else if (currentSubscription.plan_code === 'LARGE') {
+          alreadyPaid = parseFloat(LARGE_PLAN_PRICE)
+        }
+
         if (code === currentSubscription.plan_code) {
           code += '-2'
         }
@@ -93,6 +114,7 @@ export class UserController {
           }
         });
       subscriptionDate = subscriptionResult.data.subscription.subscription_at;
+      subscriptionStartedDate = new Date(subscriptionResult.data.subscription.started_at);
 
       function msleep(n) {
         Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
@@ -103,25 +125,36 @@ export class UserController {
       }
 
       let invoiceFound = false;
-      while (!invoiceFound) {
+      const maxAttempts = 3;
+      let currentAttempt = 0;
+      while (wasPreviousSubscription && !invoiceFound && currentAttempt < maxAttempts) {
         const invoicesData = await apiLago.invoices.findAllInvoices({
           external_customer_id: subscribeUserDto.wallet,
           payment_status: 'pending',
+          issuing_date_from: subscriptionStartedDate.getFullYear() + '-' + (subscriptionStartedDate.getMonth() + 1) + '-' + subscriptionStartedDate.getDate(),
         })
         if (invoicesData.data.invoices.length > 0) {
           const invoiceData = invoicesData.data.invoices[0];
           if (invoiceData.customer.lago_id === subscriptionResult.data.subscription.lago_customer_id) {
-            invoiceFound = true;
-            if (totalToPay * 100 > invoiceData.total_amount_cents)
-              totalToPay = totalToPay - (totalToPay * 100 - invoiceData.total_amount_cents) / 100;
+            // SMALL plan has recurring charge of 0.01, MEDUIM plan - 0.02, LARGE plan - 0.03
+            if (invoiceData.total_amount_cents > 3) {
+              invoiceFound = true;
+              if (totalToPay * 100 > alreadyPaid * 100 - invoiceData.total_amount_cents) {
+                totalToPay = totalToPay - (alreadyPaid * 100 - invoiceData.total_amount_cents) / 100;
+                console.log('totalToPay', totalToPay)
+              }
+            } else {
+              console.log('No customer found')
+              sleep(3);
+            }
           } else {
-            console.log('No customer found')
-            sleep(3);
+            await apiLago.invoices.updateInvoice(invoiceData.lago_id, {invoice: {payment_status: 'succeeded'}})
           }
         } else {
           console.log('No invoices found')
           sleep(3);
         }
+        currentAttempt++;
       }
     } catch (error) {
       console.log('error', error)
@@ -146,8 +179,8 @@ export class UserController {
         external_customer_id: subscribeUserDto.wallet,
         payment_status: 'pending',
       })
-      if (invoicesData.data.invoices.length > 0) {
-        const usageInvoice = invoicesData.data.invoices[0];
+      for (let i=0; i<invoicesData.data.invoices.length; i++) {
+        const usageInvoice = invoicesData.data.invoices[i];
         await apiLago.invoices.updateInvoice(usageInvoice.lago_id, {invoice: {payment_status: 'succeeded'}})
       }
       const userData = await this.userService.findUserByWallet(subscribeUserDto.wallet)
@@ -171,7 +204,7 @@ export class UserController {
         return {status: 400, statusText: 'Bad Request', data: {error: ['Invalid subscription data']}}
       }
 
-      await apiLago.subscriptions.destroySubscription({externalId: subscribeUserDto.wallet, status: 'pending'})
+      await apiLago.subscriptions.destroySubscription({externalId: 'sub_'+subscribeUserDto.wallet, status: 'pending'})
 
       const userData = await this.userService.findUserByWallet(subscribeUserDto.wallet)
       this.userService.update(userData.user.id, {plan_code: 'NONE'})
